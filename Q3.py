cca5748704065ad8991f42c6976943c3a2279957
Kesav1316 <kesavram1316@gmail.com>
2024-06-13 21:17:11 +0530

    Completed and added third task

---
+++ Q3.py
@@ -0,0 +1,43 @@
+#TASK3
+'''
+A password generator is a useful tool that generates strong and
+random passwords for users. This project aims to create a
+password generator application using Python, allowing users to
+specify the length and complexity of the password.
+'''
+
+import random
+
+l1 = [chr(i) for i in range(33,127)]
+
+def generate(length):
+    global l1
+    password = ""
+    for i in range(length):
+        rand = random.randrange(len(l1))
+        password = password + l1[rand]
+    print("The random password is ",password)
+
+print("PASSWORD GENERATOR")
+while(True):
+    print('''
+1.Generate Password
+2.Exit
+''')
+    choice = int(input("Enter the option "))
+    if choice == 1:
+        length = int(input("Enter the length of the password to generate "))
+        if(length <=0):
+            print("The length must be a positive number ")
+        
+        elif length > 0:  
+            generate(length)
+        
+        else:
+            print("Invalid. ")
+    
+    elif choice == 2:
+        break
+
+    else:
+        print("Invalid. ")
